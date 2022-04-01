390735e4e286c725e39142dac32da97dd422514b
ephong93 <ephong93@gmail.com>
2022-04-02 01:35:45 +0900

    844

---
+++ Easy/844.js
@@ -0,0 +1,19 @@
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+var backspaceCompare = function(s, t) {
+  function run(s) {
+    const arr = [];
+    s.split('').forEach(c => {
+      if (c === '#') {
+        arr.pop();
+      } else {
+        arr.push(c);
+      }
+    });
+    return arr.join();
+  }
+  return run(s) === run(t);
+};
